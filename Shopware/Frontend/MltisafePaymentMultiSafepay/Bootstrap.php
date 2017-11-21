ae6cf2f5299e9574ffaa6d32cc2b6b97508d677f
Jasper Nadi <jasper.nadi@multisafepay.com>
2017-11-21 10:29:14 +0100

    Set version to 1.0.6

--- Shopware/Frontend/MltisafePaymentMultiSafepay/Bootstrap.php
+++ Shopware/Frontend/MltisafePaymentMultiSafepay/Bootstrap.php
@@ -329,7 +329,7 @@ With MultiSafepay you can offer specific local payment options for Germany, The 
      * getVersion returns the currenct plugin version
      */
     public function getVersion() {
-        return '1.0.4';
+        return '1.0.6';
     }
 
     /**
